13bc9f9fc4293fd5cf53290d0034c3297881a210
InfinitydeGuzma <>
2021-05-10 01:33:41 +0000

    now calculates salary

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 Infinity de Guzman All rights reserved
+//
+// Created by: Infinity de Guzman
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function enterClicked () {
+  // this code will calculate your actual pay and the amount the government takes
+
+  // input
+  const hours = parseInt(document.getElementById("hours-worked").value)
+  const rate = parseInt(document.getElementById("hourly-rate").value)
+
+  // process
+  const pay = (hours x rate) * (1.00 - 0.18)
+  const government = (hours x rate) * (0.82)
+
+  //output
+  document.getElementById('pay').innerHTML = 'Your pay will be: $' + pay
+  document.getElementById('government').innerHTML = 'The government will take: $' + government
+}
